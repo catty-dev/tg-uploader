c7fe9baafeb6a8dcac5e5d28afc40f9b1c81d520
catty <c>
2023-05-19 22:04:46 +0200

    .

--- uploader.py
+++ uploader.py
@@ -2,7 +2,6 @@ import requests
 from pyrogram import Client, filters
 from pyromod import listen
 import os
-from datetime import datetime
 from configparser import ConfigParser
 import sqlite3
 
@@ -16,16 +15,15 @@ upload_url = config.get('UPLOAD', 'URL')
 
 con = sqlite3.connect("users.db")
 cur = con.cursor()
-cur.execute("create table if not exists users (userid BIGINT NOT NULL PRIMARY KEY, username TEXT, realname TEXT, id INT NOT NULL, token TEXT NOT NULL)")
+cur.execute("create table if not exists users (userid BIGINT NOT NULL PRIMARY KEY, name TEXT, id INT NOT NULL, token TEXT NOT NULL)")
 
 app = Client(
     "my_bot",
     api_id=api_id, api_hash=api_hash,
     bot_token=bot_token)
 
-now = datetime.now().strftime("%Y%m%d%H%M%S%f")
 supported_types = [".jfif" ,".webp", ".jpg", ".jpeg", ".png", ".gif", ".mp4", ".webm", ".mp3"]
-USER_PROPS = ("userid", "username", "realname", "id", "token")
+USER_PROPS = ("userid", "name", "id", "token")
 
 def get_user_db(id):
     sql = "SELECT * FROM users WHERE userid = ?"
@@ -51,58 +49,76 @@ def update_user_db(keys):
     sql = "UPDATE users SET "
     sql += ", ".join("`%s` = ?" % k for k in USER_PROPS)
     sql += " WHERE userid = ?"
-    param = list(keys) + [id, ]
+    param = list(keys) + [id]
     cur.execute(sql, param)
     con.commit()
 
 async def get_user_keys(message):
     id = await message.chat.ask('send me your id')
-    if id.text.isdigit(): token = await message.chat.ask("send me your token")
+    if id.text is not None and id.text.isdigit(): token = await message.chat.ask("send me your token")
     else: return await message.reply('invalid input')
+    if token.text is None: return await message.reply('invalid input')
     keys=get_user_tg(message)
     keys.extend([id.text, token.text])
     return keys
 
 def get_user_tg(message):
     id = message.from_user.id
-    username = message.from_user.username if message.from_user.username is not None else None
-    realname = message.from_user.first_name
-    if message.from_user.last_name is not None: realname = message.from_user.first_name + message.from_user.last_name
-    return [id, username, realname]
+    name = message.from_user.first_name
+    if message.from_user.last_name is not None: name = message.from_user.first_name + message.from_user.last_name
+    return [id, name]
+
+def check_credentials(keys):
+    id = keys[-2]
+    token = keys[-1]
+    url = upload_url
+    data={"id": id, "token": token}
+    response = requests.post(url, data=data)
+    try:
+        response.json()['status'] == 'invalid credentials'
+        return False
+    except: return True
 
 # Function to get user information
 @app.on_message(filters.command(["info"]) & filters.private)
-async def handle_document(app, message):
-    if get_user_db(message.from_user.id) == False:
+async def get_info(app, message):
+    if get_user_db(message.from_user.id) is False:
         return await message.reply('not authorized')
-    else: values = get_user_db(message.from_user.id)
-    return await message.reply(f"telegram id: {str(values[0])} \nname: {str(values[2])} \nid: {str(values[-2])} \ntoken: {str(values[-1])}")
+    values = get_user_db(message.from_user.id)
+    await message.reply(f"telegram id: {str(values[0])} \ntelegram name: {str(values[1])} \nid: {str(values[-2])} \ntoken: {str(values[-1])}")
 
 # Function to update credentials
 @app.on_message(filters.command(["update"]) & filters.private)
 async def update_credentials(app, message):
-    if get_user_db(message.from_user.id) == False:
+    if get_user_db(message.from_user.id) is False:
         return await message.reply('not authorized')
-    else: update_user_db(await get_user_keys(message))
-    return await message.reply('credentials updated')
+    user_keys = await get_user_keys(message)
+    if not isinstance(user_keys, list): return
+    if not check_credentials(user_keys): return await message.reply('invalid credentials')
+    update_user_db(user_keys)
+    await message.reply('credentials updated')
 
 # Function to handle sign up
 @app.on_message(filters.text | filters.command(["start"]) & filters.private)
 async def handle_sign_up(app, message):
-    if not get_user_db(message.from_user.id) == False:
+    if not get_user_db(message.from_user.id) is False:
         return await message.reply('send me what you want to upload to xD')
     else:
-        create_user_db(await get_user_keys(message))
-        return await message.reply('success, you can now send media to upload')
+        user_keys = await get_user_keys(message)
+        if not isinstance(user_keys, list): return
+        if not check_credentials(user_keys): return await message.reply('invalid credentials')
+        create_user_db(user_keys)
+        await message.reply('success, you can now send media to upload')
 
 # Function to handle incoming files
 @app.on_message(filters.photo | filters.video | filters.animation | filters.sticker | filters.audio | filters.document)
 async def handle_document(app, message):
-    if get_user_db(message.from_user.id) == False:
+    if get_user_db(message.from_user.id) is False:
         return await message.reply('not authorized')
-    else: values = get_user_db(message.from_user.id)
-    id = values[-2]
-    token = values [-1]
+    else:
+        keys = get_user_db(message.from_user.id)
+        id = keys[-2]
+        token = keys[-1]
 
     # Get the File object from the message
     if message.photo is not None:
@@ -125,11 +141,11 @@ async def handle_document(app, message):
     if file_object.file_size > 3e+7:
         return await message.reply('file too large')
 
-    local_file_path = await app.download_media(message)
-
-    try: await post_it(local_file_path, message, id, token)
-    except Exception as e: await message.reply("ERROR: " + e)
-    return os.remove(local_file_path)
+    try:
+        local_file_path = await app.download_media(message)
+        await post_it(local_file_path, message, id, token)
+    except Exception as e: await message.reply("ERROR: " + str(e))
+    os.remove(local_file_path)
 
 # deliver your shitpost
 async def post_it(local_file_path, message, id, token):
